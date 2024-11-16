66c40504db194c203e7434295ba5ccf029630721
BeetrootAcademyAccount3 <beetrootacademy1@yahoo.com>
2024-11-16 18:26:32 +0200

    Example arrays

---
+++ arrays.js
@@ -0,0 +1,50 @@
+// const array1 = [1, 2, 3, 4, [1, 23, 4]];
+// let array2 = array1;
+
+// console.log(array1);
+// array1.push(5);
+
+// console.log(array2);
+
+// console.log(array1[4][1]);
+
+const originalArray1 = ["Banana", "Peach", "PineApple", "Apple", "Watermelon"];
+const originalArray2 = [3, 4, 2, 1, 5];
+
+const str = "Hello";
+
+// const result = originalArray.push("Orange");
+// const result = originalArray.pop();
+// const result = originalArray.unshift("Orange");
+// const result = originalArray.shift();
+// const result = originalArray.splice(1, 2);
+// const result = originalArray.splice(2);
+// const result = originalArray.splice(1, 2, "Strawberry", "Blueberry");
+// const result = originalArray.splice(1, 0, "Strawberry", "Blueberry");
+// const result = originalArray.sort((a, b) => b - a);
+// const result = originalArray.fill("Blueberry", 1, 3);
+// const result = originalArray.fill("Blueberry", 1);
+// const result = originalArray.reverse();
+
+// const result = originalArray.at(2);
+// const result = originalArray.map((number) => {
+//   return number * 10;
+// });
+
+// const result = originalArray.filter((number) => number > 2);
+// const result = originalArray.slice(1, 3);
+
+// const result = originalArray1.concat(originalArray2);
+
+// const result = originalArray1.indexOf("Berry");
+// const result = originalArray2.findIndex((number) => number > 3);
+// const result = originalArray1.find((fruit) => fruit[0] === "P");
+// const result = originalArray1.join();
+
+// console.log(originalArray1);
+// console.log(originalArray2);
+
+// console.log(result);
+
+const result = str.split("").reverse().join("");
+console.log(result);
